6b882a9aabaec3a4daf0a100c87acdabe889d563
Susan Meerdink <susan.meerdink@gmail.com>
2017-04-13 10:15:11 -0700

    Condensing spectra extract with file discovery

--- Python/main_spectral_library.py
+++ Python/main_spectral_library.py
@@ -67,68 +67,48 @@ for fl in flList:  # loop through flightline files to find specific date
     imageLocation = dirLocation + fl + '\\6 - Spectral Correction Files\\*' + dateTag + '*'
     for name in glob.glob(imageLocation):  # Ignore header files
         if '.hdr' not in name:
-            print 'Found', name
             imageList.append(name)  # add file to list of image files for future processing
+            print 'Extracting spectra from', name
+            imgFile = rasterio.open(name, 'r')  # Open raster image
+            shortName = name.split('\\')[-1]  # Get file name
 
+            # Plot reference polygons on top of current image
+            fig, axMap = plt.subplots(num=None, figsize=(4, 3), dpi=300, facecolor='w', edgecolor='k')
+            rasterio.plot.show((imgFile, 137), ax=axMap, cmap='hot', clim=(1, 10000))  # Show band 137 of current image
+            patches = []
+            for idx, p in enumerate(polygons):
+                patches.append(PolygonPatch(p, fc='#FFFFFF', ec='#FFFFFF', alpha=1., zorder=1))
+            axMap.add_collection(PatchCollection(patches, match_original=True))
+            plt.xticks(fontsize=6)
+            plt.yticks(fontsize=6)
+            plt.title("Plant Species Reference Polygons with \n" + shortName, fontsize=6)
+            plt.savefig(outFigLoc + shortName + '_reference_poly.png', alpha=True, dpi=300)
+            plt.clf()
 
-for i in imageList:  # loop through flightline files to extract spectra
-    print 'Extracting spectra from', i
-    imgFile = rasterio.open(i, 'r')  # Open raster image
-    iName = i.split('\\')[-1]  # Get file name
+            for idx in range(0, len(polygons)):  # Loop through polygons
+                polyIn = polygons[idx]
+                polyName = polyCRS[idx]['properties']['Polygon_ID']
 
-    # Plot reference polygons on top of current image
-    fig, axMap = plt.subplots(num=None, figsize=(4, 3), dpi=300, facecolor='w', edgecolor='k')
-    rasterio.plot.show((imgFile, 137), ax=axMap, cmap='hot', clim=(1, 10000))  # Show band 137 of current image
-    patches = []
-    for idx, p in enumerate(polygons):
-        patches.append(PolygonPatch(p, fc='#FFFFFF', ec='#FFFFFF', alpha=1., zorder=1))
-    axMap.add_collection(PatchCollection(patches, match_original=True))
-    plt.xticks(fontsize=6)
-    plt.yticks(fontsize=6)
-    plt.title("Plant Species Reference Polygons with \n" + iName, fontsize=6)
-    plt.savefig(outFigLoc + iName + '_reference_poly.png', alpha=True, dpi=300)
-    plt.clf()
+                # Create Mask that has 1 for locations with polygons and 0 for non polygon locations
+                polygonMask = rasterio.features.rasterize([(polyIn, 1)], out_shape=imgFile.shape,
+                                                          transform=imgFile.transform, all_touched=True)
+                test = np.count_nonzero(polygonMask)  # Get the number of elements that are not zero
+                if test > 0:  # If there is data for this polygon assign the data
+                    indices = np.nonzero(polygonMask)
+                    for i in range(0, len(indices)):  #
+                        x = indices[0][i]
+                        y = indices[1][i]
+                        window = ((x, x + 1), (y, y + 1))
+                        data = imgFile.read(window=window, masked=False, boundless=True)  # Extract spectra from image
+                        pixel = np.transpose(data[:, 0, 0])
+                        if any(pixel):  # If there are non zero values save them to spectral library
+                            print 'here'
+                            spectralLibData = np.vstack((spectralLibData, pixel))
+                            spectralLibName = np.vstack((spectralLibName, [fl, dateTag, polyName]))
+                        else:  # If the values are all zero move on to next polygon
+                            break
+                else:  # If there is no data for this polygon skip to the next one
+                    continue
 
-    for idx in range(0, len(polygons)):  # Loop through polygons
-        polyIn = polygons[idx]
-        polyName = polyCRS[idx]['properties']['Polygon_ID']
-
-        # Create Mask that has 1 for locations with polygons and 0 for non polygon locations
-        polygonMask = rasterio.features.rasterize([(polyIn, 1)], out_shape=imgFile.shape,
-                                                  transform=imgFile.transform, all_touched=True)
-        test = np.count_nonzero(polygonMask)  # Get the number of elements that are not zero
-        if test > 0:  # If there is data for this polygon assign the data
-            indices = np.nonzero(polygonMask)
-            for i in range(0, len(indices)):  #
-                x = indices[0][i]
-                y = indices[1][i]
-                window = ((x, x+1), (y, y+1))
-                data = imgFile.read(window=window, masked=False, boundless=True)  # Extract spectra from image
-                pixel = np.transpose(data[:, 0, 0])
-                if any(pixel):  # If there are non zero values save them to spectral library
-                    print 'here'
-                    spectralLibData = np.append(spectralLibData, pixel, axis=0)
-                    spectralLibName = np.append(spectralLibName, polyName)
-                else:  # If the values are all zero move on to next polygon
-                    break
-
-            # Note: cannot read entire AVIRIS file as it is too large, need to loop by band
-            # for bandNum in range(1,225):  # Loop through bands (note: bands are indexed from 1)
-            #     masked_data = imgFile.read(bandNum)[indices]  # create a masked numpy array
-            #     if bandNum == 1:  # If this is the first band...
-            #         spectralLibName = np.append(spectralLibName, np.repeat(polyName, test))  # add polygon name to list
-            #         singleLibData = np.empty([test, 224])  # create empty numpy array
-            #         singleLibData[:, bandNum-1] = masked_data  # add data for that band to numpy array
-            #     else:  # If this is not the first band...
-            #         singleLibData[:, bandNum-1] = masked_data # add data for that band to numpy array
-            #     if bandNum == 224:  # If this is the last band add to the overall spectral library
-            #         spectralLibData = np.vstack((spectralLibData, singleLibData))
-        else:  # If there is no data for this polygon skip to the next one
-            continue
 print spectralLibName
 print np.shape(spectralLibData)
-    # Extract spectra from image
-    # Note: cannot read entire AVIRIS file as it is too large, need to loop by band
-    # for bandNum in range(1, 2):  # Loop through bands (note: bands are indexed from 1)
-        # flag = 0
-        # imgData = imgFile.read(bandNum)
